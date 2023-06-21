d3fb9308a7e4d0020b083addd0ca65e0c651bbe7
Alan <alanstvn420@gmail.com>
2023-06-21 09:01:13 -0500

    final

--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class menuPrincipal {
     public static void main(String[] args){
         Juegos1 juego1=new Indie2("awa2",123,"noc","Unreal");
-        Juegos1 juego2=new Lanzamientos9("juego"123,"noc","10 de 10");
+        Juegos1 juego2=new Lanzamientos9("juego",123,"noc","10 de 10");
 
         System.out.println(juego2.getNombre());
     }
